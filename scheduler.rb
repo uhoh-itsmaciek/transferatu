649cec3bb5bb7b57f8e3df5e93cb6cca5b3af7ed
tef <tef@heroku.com>
2016-02-12 06:59:39 -0800

    Increase throughput with enough for peak usage

--- scheduler.rb
+++ scheduler.rb
@@ -7,7 +7,11 @@ require "clockwork"
 $stdout.sync = true
 
 module Clockwork
-  # Dump 250 jobs in the queue every 5 minutes
+  # Originally: Dump 250 jobs in the queue every 5 minutes
+
+  # That's 250*12 or 3000 an hour. Peak is ~9000 jobs in one hour
+  # We will need to dump at least 850 every 5 minutes to achieve throughput
+
   # Any scheduled job that should have happened in the last 12 hours,
   # but has not been run in the last 12 hours is eligible
 
@@ -19,7 +23,7 @@ module Clockwork
     scheduled_time = Time.now
 
     Pliny.log(task: 'run-scheduled-transfers', scheduled_for: scheduled_time) do
-      manager.run_schedules(scheduled_time, 250)
+      manager.run_schedules(scheduled_time, 1000)
     end
   end
 end
